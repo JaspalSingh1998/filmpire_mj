8080766f21186e5c01ab2ac9a493461df892c749
= <=>
2022-06-02 18:00:43 +0530

    Added Basic Style setup using makeStyle from material UI

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,16 @@ import { CssBaseline } from '@mui/material';
 import {Routes, Route} from 'react-router-dom';
 
 import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
+import useStyles from './styles';
 
 function App() {
+  const classes = useStyles();  
   return (
-      <div>
+      <div className={classes.root}>
         <CssBaseline />
-        <main>
-            <NavBar />
+        <NavBar />
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Routes>
                 <Route exact path='/' element={<Movies />}/>
                 <Route exact path='/movie/:id' element={<MovieInformation />}/>
