de75319aa71aaad37ba6aabb2617adcf5e2122c4
= <=>
2022-06-02 17:50:36 +0530

    Added Dependencies, setup navigation and created main components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { CssBaseline } from '@mui/material';
+import {Routes, Route} from 'react-router-dom';
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from './index';
 
 function App() {
   return (
-    <div>App</div>
+      <div>
+        <CssBaseline />
+        <main>
+            <NavBar />
+            <Routes>
+                <Route exact path='/' element={<Movies />}/>
+                <Route exact path='/movie/:id' element={<MovieInformation />}/>
+                <Route exact path='/actors/:id' element={<Actors />}/>
+                <Route exact path='/profile/:id' element={<Profile />}/>
+            </Routes>
+        </main>
+      </div>
   )
 }
 
