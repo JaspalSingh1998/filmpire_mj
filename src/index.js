cc94d639e604be909ae5ee789e25c82ce1477a9a
= <=>
2022-06-03 15:17:44 +0530

    Added Navbar layout using MUI

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
+import {createTheme} from '@mui/material';
+import {ThemeProvider} from '@mui/styles';
 
 import App from './components/App';
+
+const theme = createTheme({});
+
 
 const root = ReactDOM.createRoot(
     document.getElementById("root")
   );
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </ThemeProvider>
 );
