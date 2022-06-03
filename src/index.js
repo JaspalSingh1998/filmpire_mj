44279d763570c69c6468c605614162c956d09560
= <=>
2022-06-03 18:08:42 +0530

    added redux setup and popular movie api

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import {createTheme} from '@mui/material';
 import {ThemeProvider} from '@mui/styles';
+import { Provider } from 'react-redux';
 
+import store from './app/store';
 import App from './components/App';
 
 const theme = createTheme({});
@@ -13,9 +15,11 @@ const root = ReactDOM.createRoot(
     document.getElementById("root")
   );
 root.render(
-    <ThemeProvider theme={theme}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
+    </Provider>
 );
